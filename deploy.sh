6424ecbb515f53ce355f13ed75e0d6edb69d0b97
Wouter in 't Veld <wouter.in.t.veld@knmi.nl>
2021-11-22 08:58:13 +0100

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -3,10 +3,11 @@
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
 # Build the project.
-hugo
+hugo -D
 
 # Go To Public folder
 pushd public
+
 # Add changes to git.
 git add .
 
@@ -15,9 +16,10 @@ msg="rebuilding site `date`"
 if [ $# -eq 1 ]
   then msg="$1"
 fi
+
 git commit -m "$msg"
 
 # Push source and build repos.
-git push origin gh-pages
+git push origin HEAD:gh-pages
 
 popd
